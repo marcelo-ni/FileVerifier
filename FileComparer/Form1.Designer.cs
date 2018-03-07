90090580f1f81146765df5122a494e5d6ca7344e
marcelo-ni <marcelo.nicolet@gmail.com>
2018-03-07 13:37:45 -0300

    Initial commit

---
+++ FileComparer/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace FileComparer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sourceButt = new System.Windows.Forms.Button();
+            this.sourceDir = new System.Windows.Forms.TextBox();
+            this.sourceInclude = new System.Windows.Forms.CheckBox();
+            this.targetInclude = new System.Windows.Forms.CheckBox();
+            this.targetDir = new System.Windows.Forms.TextBox();
+            this.targetButt = new System.Windows.Forms.Button();
+            this.goButt = new System.Windows.Forms.Button();
+            this.resulList = new System.Windows.Forms.ListBox();
+            this.qtyText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sourceButt
+            // 
+            this.sourceButt.Location = new System.Drawing.Point(48, 27);
+            this.sourceButt.Name = "sourceButt";
+            this.sourceButt.Size = new System.Drawing.Size(75, 23);
+            this.sourceButt.TabIndex = 0;
+            this.sourceButt.Text = "Source";
+            this.sourceButt.UseVisualStyleBackColor = true;
+            this.sourceButt.Click += new System.EventHandler(this.sourceButt_Click);
+            // 
+            // sourceDir
+            // 
+            this.sourceDir.Location = new System.Drawing.Point(152, 28);
+            this.sourceDir.Name = "sourceDir";
+            this.sourceDir.Size = new System.Drawing.Size(358, 20);
+            this.sourceDir.TabIndex = 1;
+            // 
+            // sourceInclude
+            // 
+            this.sourceInclude.AutoSize = true;
+            this.sourceInclude.Location = new System.Drawing.Point(531, 30);
+            this.sourceInclude.Name = "sourceInclude";
+            this.sourceInclude.Size = new System.Drawing.Size(97, 17);
+            this.sourceInclude.TabIndex = 2;
+            this.sourceInclude.Text = "Include subdirs";
+            this.sourceInclude.UseVisualStyleBackColor = true;
+            // 
+            // targetInclude
+            // 
+            this.targetInclude.AutoSize = true;
+            this.targetInclude.Location = new System.Drawing.Point(532, 64);
+            this.targetInclude.Name = "targetInclude";
+            this.targetInclude.Size = new System.Drawing.Size(97, 17);
+            this.targetInclude.TabIndex = 5;
+            this.targetInclude.Text = "Include subdirs";
+            this.targetInclude.UseVisualStyleBackColor = true;
+            // 
+            // targetDir
+            // 
+            this.targetDir.Location = new System.Drawing.Point(152, 62);
+            this.targetDir.Name = "targetDir";
+            this.targetDir.Size = new System.Drawing.Size(358, 20);
+            this.targetDir.TabIndex = 4;
+            // 
+            // targetButt
+            // 
+            this.targetButt.Location = new System.Drawing.Point(49, 61);
+            this.targetButt.Name = "targetButt";
+            this.targetButt.Size = new System.Drawing.Size(75, 23);
+            this.targetButt.TabIndex = 3;
+            this.targetButt.Text = "Target";
+            this.targetButt.UseVisualStyleBackColor = true;
+            this.targetButt.Click += new System.EventHandler(this.targetButt_Click);
+            // 
+            // goButt
+            // 
+            this.goButt.Location = new System.Drawing.Point(49, 105);
+            this.goButt.Name = "goButt";
+            this.goButt.Size = new System.Drawing.Size(75, 23);
+            this.goButt.TabIndex = 6;
+            this.goButt.Text = "Go";
+            this.goButt.UseVisualStyleBackColor = true;
+            this.goButt.Click += new System.EventHandler(this.goButt_Click);
+            // 
+            // resulList
+            // 
+            this.resulList.FormattingEnabled = true;
+            this.resulList.Location = new System.Drawing.Point(152, 140);
+            this.resulList.Name = "resulList";
+            this.resulList.Size = new System.Drawing.Size(629, 173);
+            this.resulList.TabIndex = 7;
+            this.resulList.Click += new System.EventHandler(this.resulList_Click);
+            // 
+            // qtyText
+            // 
+            this.qtyText.Location = new System.Drawing.Point(152, 106);
+            this.qtyText.Name = "qtyText";
+            this.qtyText.Size = new System.Drawing.Size(157, 20);
+            this.qtyText.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 338);
+            this.Controls.Add(this.qtyText);
+            this.Controls.Add(this.resulList);
+            this.Controls.Add(this.goButt);
+            this.Controls.Add(this.targetInclude);
+            this.Controls.Add(this.targetDir);
+            this.Controls.Add(this.targetButt);
+            this.Controls.Add(this.sourceInclude);
+            this.Controls.Add(this.sourceDir);
+            this.Controls.Add(this.sourceButt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sourceButt;
+        private System.Windows.Forms.TextBox sourceDir;
+        private System.Windows.Forms.CheckBox sourceInclude;
+        private System.Windows.Forms.CheckBox targetInclude;
+        private System.Windows.Forms.TextBox targetDir;
+        private System.Windows.Forms.Button targetButt;
+        private System.Windows.Forms.Button goButt;
+        private System.Windows.Forms.ListBox resulList;
+        private System.Windows.Forms.TextBox qtyText;
+    }
+}
+
---
+++ FileComparer/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FileComparer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            goButt.Enabled = false;
+        }
+
+        bool selectedSource = false;
+        bool selectedTarget = false;
+
+        private void sourceButt_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog sourceBrowser = new FolderBrowserDialog();
+            if (!String.IsNullOrEmpty(sourceDir.Text))
+                sourceBrowser.SelectedPath = sourceDir.Text;
+            DialogResult res = sourceBrowser.ShowDialog();
+            if (res == DialogResult.OK) {
+                sourceDir.Text = sourceBrowser.SelectedPath;
+                selectedSource = true;
+                checkCanGo();
+            }
+            sourceBrowser.Dispose();
+        }
+
+
+
+        private void targetButt_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog targetBrowser = new FolderBrowserDialog();
+            if (!String.IsNullOrEmpty(targetDir.Text))
+                targetBrowser.SelectedPath = targetDir.Text;
+            DialogResult res = targetBrowser.ShowDialog();
+            if (res == DialogResult.OK) {
+                targetDir.Text = targetBrowser.SelectedPath;
+                selectedTarget = true;
+                checkCanGo();
+            }
+            targetBrowser.Dispose();
+        }
+
+        void checkCanGo()
+        {
+            if (selectedSource && selectedTarget) {
+                if (sourceDir.Text != targetDir.Text)
+                    goButt.Enabled = true;
+            }
+        }
+        
+        List<string> getFiles( string path )
+        {
+            return new List<string>(Directory.EnumerateFiles(path));
+        }
+
+        List<string> getDirs( string path )
+        {
+            return new List<string>(Directory.EnumerateDirectories(path));
+        }
+
+        List<string> getDirectoryContent( string path, bool incsubdirs)
+        {
+            List<string> result = new List<string>();
+
+            if (!incsubdirs) { 
+                return getFiles(path);
+            }
+            else {
+                List<string> filelist = getFiles(path);
+                if (filelist.Count > 0)
+                    result.AddRange(filelist);
+                List<string> dirlist = getDirs(path);
+                if (dirlist.Count > 0) { 
+                    foreach (string dir in dirlist) {
+                        result.AddRange(getDirectoryContent(dir, incsubdirs));
+                    }
+                }
+            }
+       
+            return result;
+        }
+
+        List<string> reduce( string path, List<string> target )
+        {
+            List<string> result = new List<string>();
+            string testname = Path.GetFileName(path);
+            foreach (string now in target) {
+                string namenow = Path.GetFileName(now);
+                if (namenow == testname)
+                    result.Add(now);
+            }
+            return result;
+        }
+
+        enum SearchResult
+        {
+            Found = 1,
+            NameNotFound = 2,
+            FilesDiffer,
+        }
+
+        bool equalDates( DateTime l, DateTime r)
+        {
+            if (l.Year == r.Year && l.Month == r.Month && l.Day == r.Day)
+                return true;
+            return false;
+        }
+
+        SearchResult checkFile( string path, List<string> target, out FileInfo testinfo, out int count)
+        {
+            string testname = Path.GetFileName(path);
+            count = 0;
+            testinfo = new FileInfo(path);
+            List<string> reduced = reduce(path, target);
+            count = reduced.Count;
+            if (reduced.Count == 0)
+                return SearchResult.NameNotFound;
+            foreach (string now in reduced) {
+                //string namenow = Path.GetFileName(now);
+                //if (namenow != testname)
+                //    continue;
+                FileInfo infonow = new FileInfo(now);
+                if ( equalDates( infonow.LastWriteTime, testinfo.LastWriteTime) && infonow.Length == testinfo.Length)
+                    return SearchResult.Found;                
+            }
+            return SearchResult.FilesDiffer;
+        }
+
+        List<string> sourceList;
+        List<string> targetList;
+
+        private void goButt_Click(object sender, EventArgs e)
+        {
+            sourceList = getDirectoryContent(sourceDir.Text, sourceInclude.Checked);
+            targetList = getDirectoryContent(targetDir.Text, targetInclude.Checked);
+
+            int notfound = 0;
+            resulList.Items.Clear();
+            FileInfo info;
+            int count = 0;
+            foreach (string path in sourceList) {
+                SearchResult res = checkFile(path, targetList, out info, out count);
+                if ( res == SearchResult.Found )
+                    continue;
+                string brief = res == SearchResult.NameNotFound? "N" : "D";
+                string toadd = String.Format("{3} {4} {0:yyyy-MM-dd} {1,15} {2}", info.CreationTime, info.Length, path, brief, count);
+                resulList.Items.Add(toadd);
+                notfound++;
+                
+            }
+
+            qtyText.Text = String.Format("{0} / {1}", notfound, sourceList.Count);
+            
+        }
+
+        private void resulList_Click(object sender, EventArgs e)
+        {
+            string item = resulList.Items[resulList.SelectedIndex].ToString();
+            string[] parts = item.Split(new char[] { ' ' });
+            string path = parts[parts.Length - 1];
+            List<string> reduced = reduce(path, targetList);
+            StringBuilder sb = new StringBuilder();
+            foreach (string tgt in reduced) {
+                FileInfo info = new FileInfo( tgt );
+                sb.AppendFormat("{0:yyyy-MM-dd} {1,15} {2}{3}", info.LastWriteTime, info.Length, tgt, Environment.NewLine);
+            }
+            MessageBox.Show( sb.ToString());
+        }
+
+
+    }
+}
